71a80cbd972d4b8ae244eaaa486d641bb9e45dcc
Waltton Morais <walttonm@gmail.com>
2017-10-25 23:51:37 -0200

    Add server graceful shutdown

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	"github.com/waltton/go-zero-downtime/config"
 	"github.com/waltton/go-zero-downtime/server"
 )
+
+func waitSignal(ctx context.Context) func() error {
+	return func() error {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case sig := <-sigs:
+			return fmt.Errorf("signal '%s' received", sig)
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
 
 func untilFail(wg *sync.WaitGroup, cancel context.CancelFunc, f func() error) {
 	err := f()
@@ -30,6 +48,9 @@ func main() {
 
 	var wg sync.WaitGroup
 	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go untilFail(&wg, cancel, waitSignal(ctx))
 
 	wg.Add(1)
 	srv := server.New(ctx, cfg.Server)
--- server/server.go
+++ server/server.go
@@ -3,27 +3,81 @@ package server
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/waltton/go-zero-downtime/config"
 	"github.com/waltton/go-zero-downtime/handler"
 )
 
 type Server struct {
-	ctx     context.Context
-	cfg     *config.Server
-	handler http.Handler
+	ctx context.Context
+	cfg *config.Server
+	srv http.Server
 }
 
 func New(ctx context.Context, cfg *config.Server) *Server {
 	handler := handler.New()
-	return &Server{ctx, cfg, handler}
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Handler: handler,
+	}
+
+	return &Server{ctx, cfg, srv}
+}
+
+func (s Server) Shutdown() error {
+	<-s.ctx.Done()
+
+	shutdownInterval := time.Second * time.Duration(s.cfg.IntervalToShutdown)
+	log.Printf("Shutting down the server with timeout of %s...\n", shutdownInterval)
+
+	ctx, cancel := context.WithTimeout(s.ctx, shutdownInterval)
+	defer cancel()
+
+	err := s.srv.Shutdown(ctx)
+	log.Println("Shutdown err", err)
+	return err
 }
 
 func (s Server) Run() error {
-	srv := http.Server{
-		Addr:    fmt.Sprintf("%s:%s", s.cfg.Host, s.cfg.Port),
-		Handler: s.handler,
-	}
-	return srv.ListenAndServe()
+	var wg sync.WaitGroup
+	var err error
+	wg.Add(2)
+
+	exit := make(chan bool)
+
+	go func() {
+		defer wg.Done()
+
+		select {
+		case <-exit:
+			return
+		case <-s.ctx.Done():
+		}
+
+		shutdownInterval := time.Second * time.Duration(s.cfg.IntervalToShutdown)
+		log.Printf("Shutting down the server with timeout of %s...\n", shutdownInterval)
+
+		ctx, cancel := context.WithTimeout(context.Background(), shutdownInterval)
+		defer cancel()
+
+		if err := s.srv.Shutdown(ctx); err != nil {
+			log.Printf("Error while shuting down the server, error %s\n", err)
+		}
+	}()
+
+	go func() {
+		err = s.srv.ListenAndServe()
+		if err != http.ErrServerClosed {
+			exit <- true
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	return err
 }
